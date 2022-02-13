6b66a5fa944917f8ada11ec2c019ef552e0ff19c
1Mill <erik.ekberg@outlook.com>
2022-02-13 13:21:50 -0800

    Formatting

--- src/views/01-learning-vue-3.vue
+++ src/views/01-learning-vue-3.vue
@@ -1,46 +1,45 @@
 <script setup>
-import { onMounted, ref, watch } from 'vue'
+	import { onMounted, ref, watch } from 'vue'
 
-const props = defineProps({
-	someValue: {
-		default: 123,
-		type: [Number, String],
-	}
-})
+	const props = defineProps({
+		someValue: {
+			default: 123,
+			type: [Number, String],
+		}
+	})
 
-// * Counter logic
-const count = ref(parseInt(props.someValue))
+	// * Counter logic
+	const count = ref(parseInt(props.someValue))
 
-const increment = () => count.value++
-const reset = () => count.value = 0
+	const increment = () => count.value++
+	const reset = () => count.value = 0
 
-watch(count, () => console.log('Count Changed to: ', count.value))
+	watch(count, () => console.log('Count Changed to: ', count.value))
 
-// * Auto focus input
-const countInput = ref(null)
-onMounted(() => countInput.value.focus() )
+	// * Auto focus input
+	const countInput = ref(null)
+	onMounted(() => countInput.value.focus() )
 </script>
-
 <template>
-<button @click="increment">
-	INCREMENT
-</button>
-<button @click="reset">
-	RESET
-</button>
-<button @click="count = 100">
-	Set to 100
-</button>
-<p>
-	<span :class="$style.example">{{ count }}</span>
-</p>
-<input v-model="count" type="number" ref="countInput"/>
+	<button @click="increment">
+		INCREMENT
+	</button>
+	<button @click="reset">
+		RESET
+	</button>
+	<button @click="count = 100">
+		Set to 100
+	</button>
+	<p>
+		<span :class="$style.example">{{ count }}</span>
+	</p>
+	<input v-model="count" type="number" ref="countInput"/>
 </template>
 <style module>
-.example {
-	border-radius: 100%;
-	border: 1px solid black;
-	font-size: 1.5rem;
-	padding: 1rem;
-}
+	.example {
+		border-radius: 100%;
+		border: 1px solid black;
+		font-size: 1.5rem;
+		padding: 1rem;
+	}
 </style>
