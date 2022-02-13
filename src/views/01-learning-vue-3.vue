4a77868903d5a422882f770f24c3c35ea0ae4184
1Mill <erik.ekberg@outlook.com>
2022-02-13 10:10:07 -0800

    Update 01-learning-vue-3.vue

--- src/views/01-learning-vue-3.vue
+++ src/views/01-learning-vue-3.vue
@@ -1,16 +1,25 @@
 <script setup>
-import { ref } from 'vue'
-
+import { ref, watch } from 'vue'
 
 const count = ref(0)
 
-function increment() {
-	count.value++
-}
+const increment = () => count.value++
+const reset = () => count.value = 0
+
+watch(count, () => {
+	console.log('Count Changed to: ', count.value)
+})
 </script>
 
 <template>
 <button @click="increment">
-	COUNT {{ count }}
+	INCREMENT
 </button>
+<button @click="reset">
+	RESET
+</button>
+<button @click="count = 100">
+	Set to 100
+</button>
+<p>{{ count }}</p>
 </template>
