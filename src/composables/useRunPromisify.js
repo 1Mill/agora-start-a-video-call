21716de21ef3aac428d6c808f01be15c38051507
1Mill <erik.ekberg@outlook.com>
2022-02-15 20:53:38 -0800

    Clean up files

--- src/composables/useRunPromisify.js
+++
@@ -1,6 +0,0 @@
-import { promisify } from 'util'
-
-export const runPromisify = async (func, ...params) => {
-	const res = await (promisify(func))(params)
-	return res
-}
--- src/views/03-toggle-join-call.vue
+++ src/views/03-toggle-join-call.vue
@@ -1,7 +1,5 @@
 <script setup>
-	import { useAgoraClient } from '@/composables/useAgoraClient.js'
-	const { client } = await useAgoraClient()
-	console.log(client)
+	console.log("TODO")
 </script>
 <template>
 	<h1>Toggle Join Call</h1>
